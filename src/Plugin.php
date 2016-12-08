ebdc653e1203137b737181e37fbd2e7e194be8ea
Sven Wittevrongel <cupoftea696@gmail.com>
2016-12-08 11:57:33 +0000

    Release: v1.0.1

--- src/Plugin.php
+++ src/Plugin.php
@@ -32,7 +32,7 @@ class Plugin implements PluginInterface, EventSubscriberInterface
      *
      * @const string
      */
-    const VERSION = '1.0.0';
+    const VERSION = '1.0.1';
     
     /**
      * The composer instance.
