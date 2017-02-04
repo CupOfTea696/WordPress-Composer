c19da3bbe1d1f08868410a6dcc08e0576939d0c0
CupOfTea696 <cupoftea696@gmail.com>
2017-02-04 18:57:51 +0000

    Release: v1.0.7

--- src/Plugin.php
+++ src/Plugin.php
@@ -32,7 +32,7 @@ class Plugin implements PluginInterface, EventSubscriberInterface
      *
      * @const string
      */
-    const VERSION = '1.0.6';
+    const VERSION = '1.0.7';
     
     /**
      * The composer instance.
